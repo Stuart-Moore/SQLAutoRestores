d64feff50cafa882fa0273c66ea8aa13bab8eea0
Stuart Moore <stuart@stuart-moore.com>
2016-07-18 17:58:56 +0100

    more small changes

--- SQLAutoRestores.psm1
+++ SQLAutoRestores.psm1
@@ -8,7 +8,12 @@ Pop-Location
 . $PSScriptRoot\function-Get-RandomElement.ps1     
 . $PSScriptRoot\function-Get-RestoreSet.ps1        
 . $PSScriptRoot\function-Remove-Database.ps1       
-. $PSScriptRoot\function-Invoke-RestoreDatabase.ps1      
+. $PSScriptRoot\function-Restore-Database.ps1      
 . $PSScriptRoot\function-Test-Database.ps1         
 . $PSScriptRoot\function-Test-DatabaseVersion.ps1  
-. $PSScriptRoot\function-Test-RestoreSpace.ps1     
+. $PSScriptRoot\function-Test-RestoreSpace.ps1  
+. $PSScriptRoot\function-Restore-SQLBackupHeader.ps1
+. $PSScriptRoot\function-Test-BackupObject.ps1
+. $PSScriptRoot\function-New-BackupObject.ps1   
+. $PSScriptRoot\function-Get-PointInTime.ps1
+. $PSScriptRoot\function-New-SQLConnection.ps1
