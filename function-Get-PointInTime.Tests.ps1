adc86a21cc108c270633327bf6aad7394c7b19b0
Stuart Moore <stuart.moore@ntu.ac.uk>
2016-07-19 15:21:37 +0100

    Fixing up Get-PointInTime
    fixed logic problems and a couple of potential fail points
    Added unit tests for Get-PointInTime as well.

--- function-Get-PointInTime.Tests.ps1
+++ function-Get-PointInTime.Tests.ps1
@@ -5,22 +5,34 @@ Import-Module .\SQLAutoRestores -Force
 
 Describe 'Get-PointInTime Unit Tests' -tags 'Unit' {
     InModuleScope SQLAutoRestores {
-        Context "Single Folder at bottom" {
+        Context "Pushing in Date Tests" {
             $tmp = New-BackupObject
             $t = get-date '19 July 2016 13:53:19'
             $tmp.StartDate = $t
-            $tmp.FinishDate = $t.AddMilliseconds(2)
             $tmp.BackupType = 'Database'
+            $tmp.FinishDate = $t.AddMilliseconds(2)
             It "should return DateTime "{
                 Get-PointInTime $tmp | Should beofType DateTime
             }
             #Cheating here! Need to add milliseconds to get a value, but rounded out as we return at second resolution
-            It "Should return time" {
+            It "Should return This Time" {
                 Get-PointInTime $tmp | Should Be '07/19/2016 13:53:19'
             }
-            It "Should barf given not-dates" {
-                
+            $tmp.FinishDate = $t
+            It "Should return Endtime if both times same" {
+                Get-PointInTime $tmp | Should Be '07/19/2016 13:53:19'
             }
+            It "Should Barf if enddate not dates" {
+                $tmp.FinishDate='hello'
+                $tmp.StartDate = $t
+                { Get-PointInTime $tmp } | Should Throw "not a datetime"
+            }
+            It "Should Barf if startdate not dates" {
+                $tmp.StartDate='hello'
+                $tmp.FinishDate = $t.AddMilliseconds(2)
+                { Get-PointInTime $tmp } | Should Throw "not a datetime"
+            }
+
         }
     }
 }
--- function-Get-PointInTime.ps1
+++ function-Get-PointInTime.ps1
@@ -20,8 +20,9 @@
   )
   Write-Verbose "Get-PointInTime - Entering"
   Write-Verbose "Get-PointInTime - Checking Dates"
-    $StartDate = $BackupsObject | Where-Object {$_.BackupType -eq 'Database'} | Measure-Object -Min -Property startdate    $EndDate = $BackupsObject.FinishDate | Measure-Object -Maxif ($StartDate -eq $EndDate){    Write-Verbose "Get-PointInTime - DateTimes are the same, random call will fail, so return Endate"}else{    $seconds = Get-Random ($EndDate.Maximum -$StartDate.Minimum).TotalSeconds    $TargetTime = $startdate.Minimum.AddSeconds($seconds)
+    $StartDate = $BackupsObject | Where-Object {$_.BackupType -eq 'Database'} | Measure-Object -Min -Property startdate    $EndDate = $BackupsObject.FinishDate | Measure-Object -Maximumif (!($startdate.Minimum -is [dateTime])){    Write-Verbose "Get-PointInTime - Startdate is not a datetime, exiting"    Throw "Startdate is not a datetime"}if (!($EndDate.Maximum -is [dateTime])){    Write-Verbose "Get-PointInTime - EndDate is not a datetime, exiting"    Throw "Enddate is not a datetime"}if ($StartDate.minimum -eq $EndDate.Maximum){    Write-Verbose "Get-PointInTime - DateTimes are the same, random call will fail, so return Endate"    return $Enddate.Maximum}else{    $seconds = Get-Random ($EndDate.Maximum -$StartDate.Minimum).TotalSeconds    #$seconds = get-random ($EndDate - $StartDate).totalseconds
+    $TargetTime = $startdate.Minimum.AddSeconds($seconds)
     return $TargetTime
-    Write-Verbose "Get-PointInTime - Leaving"
     }
+    Write-Verbose "Get-PointInTime - Leaving"
  }
